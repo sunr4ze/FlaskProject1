2abb6d873ebb4f820acaad34f21f69bb846e8f08
sunr4zee <qwfqfqwqdqdq@gmail.com>
2023-02-13 16:39:10 +0300

    Work with the page to display articles

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page' + ' ' + name +  ' ' + '- ' + str(id)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method=='POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("create-article.html")
+
+
+@app.route('/news')
+def news():
+    articles = Article.query.order_by(Article.date).all()
+    return render_template("news.html", articles=articles)
+
+
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
